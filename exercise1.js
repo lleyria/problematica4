1f8c312f09ee150672d5a87107ca607f1f6c8688
Lisandro Leyria <l.leyria93@gmail.com>
2020-10-24 21:19:12 -0300

    First Java Script exercise

---
+++ exercise1.js
@@ -0,0 +1,20 @@
+//Excercise 1.a//
+var sum1, sum2, sum;
+sum1=1;
+sum2=1;
+sum = sum1 + sum2;
+console.log(sum);
+
+//Exercise 1.b//
+var word1, word2, newWord;
+word1='Hello';
+word2= 'World';
+newWord = word1 + word2;
+console.log(newWord);
+
+//Exercise 1.c//
+var word3, word4, long;
+word3 = 'Goodbye';
+word4 = 'Moonmen'
+long = word3.length + word4.length;
+console.log(long);
